2ca016b808697453b29dfcf9ffe631be7c057794
DeLxCruz <lopezsantiago3503@gmail.com>
2023-08-28 13:24:52 -0500

    feat:🔧: Se añade peticion fetch remota

--- src/main.js
+++ src/main.js
@@ -1,20 +1,25 @@
-const url = '../data/dataVideos.json';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '8daaabf2damshe677dfe21e759bfp1713a7jsnf88d97a4cb2d',
-// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-// 	}
-// };
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8daaabf2damshe677dfe21e759bfp1713a7jsnf88d97a4cb2d',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
 
-const asyncFunction = async (url) => {
+{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/6GLXWseOGmQ?si=wov4iC3MMPARIVtq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */ }
 
+const asyncFunction = async (p1) => {
+    options.method = 'GET'
+    // const url = await fetch(`https://youtube138.p.rapidapi.com/channel/search/?id=UCJ5v_MCY6GNUBTO8-D3XoAg&q=${p1}&hl=en&gl=US`, options)
+    const url = '../data/dataVideos.json';
     const response = await fetch(url);
     const result = await response.json();
     console.log(result);
 
+    let height = 0, cont = 0;
+
     let banner = document.querySelector(".container")
-        banner.insertAdjacentHTML("beforeend", /*HTML*/`
+    banner.insertAdjacentHTML("beforeend", /*HTML*/`
         <div class="list-container">
             ${result.contents.map((value) => /*HTML*/`
                 <div class="vid-list">
@@ -32,22 +37,22 @@ const asyncFunction = async (url) => {
             `).join("")}
         </div>
     `)
-    
-    
+
+
     const info = document.querySelectorAll(".vid-list")
-        
-    info.forEach(video_info =>{
+
+    info.forEach(video_info => {
         const imagen_video = video_info.querySelector('.imagen-principal');
         const titulo = imagen_video.getAttribute('data-video-titulo');
 
-        imagen_video.addEventListener('click', () =>{
+        imagen_video.addEventListener('click', () => {
             const videoSeleccionado = obtenerVideoInfo(result, titulo);
             localStorage.setItem('videoSeleccionado', JSON.stringify(videoSeleccionado));
             window.location.href = '';
         })
     })
     console.log(localStorage);
-    function obtenerVideoInfo(videos,title){
+    function obtenerVideoInfo(videos, title) {
         const videoVideo = videos.contents.find(v => v.title = title)
         return videoVideo
     }
@@ -55,7 +60,7 @@ const asyncFunction = async (url) => {
 }
 
 
-asyncFunction(url)
+asyncFunction()
 
 const toggleSidebarBtn = document.getElementById('toggleSidebar');
 const sidebar = document.querySelector('aside');
@@ -64,14 +69,8 @@ const iconTexts = document.querySelectorAll('.icon-text');
 toggleSidebarBtn.addEventListener('click', () => {
     sidebar.classList.toggle('w-16');
     sidebar.classList.toggle('w-52');
-    
+
     iconTexts.forEach(iconText => {
         iconText.classList.toggle('hidden');
     });
 });
-
-
-
-
-
-
